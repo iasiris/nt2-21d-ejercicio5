3753d74bc5448b20dd213822e2c5c3816e7d7e43
iasiris <baenamariana@gmail.com>
2021-06-21 12:43:17 -0300

    mariana baena

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,119 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header :rgbString="colorDisplayText" :bgcolor="headerBackgroundColor" />
+    <Navbar
+      :rbText="restartButtonText"
+      @onRestart="restart"
+      :txtContent="messageDisplayText"
+      :isHard="getIsHard()"
+      @onDifChanged="difChanged($event)"
+    />
+    <Main :colores="colors" @onCheckSelection="checkSelection($event)" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from "./components/Navbar.vue";
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Navbar,
+    Header,
+    Main,
+  },
+  methods: {
+    getIsHard() {
+      return this.isHard;
+    },
+    difChanged(isHard) {
+      this.isHard = isHard;
+      this.colorCount = isHard ? 6 : 3;
+      this.restart();
+    },
+    PickColor() {
+      var quantity;
+      if (this.isHard) {
+        quantity = 6;
+      } else {
+        quantity = 3;
+      }
+      return Math.floor(Math.random() * quantity);
+    },
+    createNewColors(numbers) {
+      var arr = [];
+      for (var i = 0; i < numbers; i++) {
+        arr.push(this.createRandomStringColor());
+      }
+      return arr;
+    },
+    createRandomStringColor() {
+      var newColor =
+        "rgb(" +
+        this.randomInt() +
+        ", " +
+        this.randomInt() +
+        ", " +
+        this.randomInt() +
+        ")";
+      return newColor;
+    },
+    randomInt() {
+      return Math.floor(Math.random() * 256);
+    },
+    restart() {
+      this.colors = this.createNewColors(this.colorCount);
+      this.pickedColor = this.colors[this.PickColor()];
+      this.colorDisplayText = this.pickedColor;
+      this.textContent = "Pick New Colors!";
+      this.headerBackgroundColor = "steelblue";
+      this.messageDisplayText = "";
+      this.restartButtonText = "New Colors!";
+    },
+    setAllColorsTo(color) {
+      let newColors = [];
+      for (let i = 0; i < this.colorCount; i++) {
+        newColors.push(color);
+      }
+      this.colors = newColors;
+    },
+    checkSelection(clickedSquare) {
+      let clickedColor = this.colors[clickedSquare];
+      if (clickedColor === this.pickedColor) {
+        this.messageDisplayText = "You Picked Right!";
+        this.setAllColorsTo(this.pickedColor);
+        this.restartButtonText = "Play Again!";
+        this.headerBackgroundColor = this.pickedColor;
+      } else {
+        this.colors.splice(clickedSquare, 1, "#232323");
+        this.messageDisplayText = "Try Again!";
+      }
+    },
+  },
+  data() {
+    return {
+      colorCount: 6,
+      isHard: true,
+      colors: [],
+      pickedColor: 0,
+      colorDisplayText: "",
+      headerBackgroundColor: "",
+      restartButtonText: "",
+      messageDisplayText: "",
+    };
+  },
+  mounted() {
+    this.restart();
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: #232323;
+  margin: 0;
+  font-family: "Montserrat", "Avenir";
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,73 @@
+<template>
+  <section class="src-components-navbar">
+    <button id="reset" @click="$emit('onRestart')">{{ rbText }}</button>
+    <span id="message">{{ txtContent }}</span>
+
+    <button id="easy" @click="setEasy()" :class="isHard ? '' : 'selected'">
+      easy
+    </button>
+    <button id="hard" @click="setHard()" :class="isHard ? 'selected' : ''">
+      hard
+    </button>
+  </section>
+</template>
+
+<script lang="js">
+  export default  {
+    name: 'src-components-navbar',
+    props: ['rbText','txtContent','isHard'],
+    mounted () {
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+setHard() {
+        this.$emit('onDifChanged',true)
+      },
+      setEasy() {
+        this.$emit('onDifChanged',false)
+      }
+
+    },
+    computed: {
+    }
+}
+</script>
+
+<style scoped lang="css">
+.src-components-navbar {
+  background: #ffffff;
+  height: 30px;
+  text-align: center;
+  margin: 0;
+  margin-top: -30px;
+}
+.selected {
+  background-color: steelblue;
+  color: white;
+}
+button {
+  border: none;
+  background-color: white;
+  font-family: "Montserrat", "Avenir";
+  text-transform: uppercase;
+  height: 100%;
+  font-weight: 700;
+  letter-spacing: 1px;
+  color: steelblue;
+  transition: all 0.3s;
+  outline: none;
+}
+button:hover {
+  color: white;
+  background-color: steelblue;
+}
+#message {
+  color: #000000;
+  display: inline-block;
+  width: 20%;
+}
+</style>
